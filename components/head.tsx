0331565dcb7b4a7f00b7be1ec2c4ced6a41f9b38
Kei <0xkeiw@gmail.com>
2022-02-10 16:32:24 +0900

    fix favicon size

--- components/head.tsx
+++ components/head.tsx
@@ -12,7 +12,7 @@ export default function Head() {
           desc="Kei's corner"
           url="https://keiw.xyz"
           image="https://github.com/wkei.png"
-          icon="https://github.com/wkei.png"
+          icon="https://github.com/wkei.png?size=48"
         />
       </NextHead>
     </>
